0722c3d3b41a5f0d76a52099a548475e446b9793
Hieu <hieunguyen.0527@gmail.com>
2023-10-22 00:37:21 -0400

    init cparse

---
+++ cparse/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hieu Nguyen on 10/22/23.
+//
+
+#include "test.h"
---
+++ cparse/test.h
@@ -0,0 +1,19 @@
+//
+// Created by Hieu Nguyen on 10/22/23.
+//
+
+#ifndef TELEMETRY_PROCESSOR_TEST_H
+#define TELEMETRY_PROCESSOR_TEST_H
+
+#include <iostream>
+using namespace std;
+
+class test {
+public:
+    test() {
+        cout << "hello world!";
+    }
+};
+
+
+#endif //TELEMETRY_PROCESSOR_TEST_H
